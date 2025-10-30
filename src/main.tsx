b2ca052e5f4e039b0135ef1ee6bfb10fef29522c
thee <theekshana.sandaru@gmail.com>
2025-10-30 18:08:00 +0100

    updated

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  // <HelmetProvider>
-  <App />
-  // </HelmetProvider>
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>
 );
